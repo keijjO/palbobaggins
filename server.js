6092984f890f652c14cbf243abfa72f889ec7ff1
Miska Melkinen <miska.melkinen@student.tut.fi>
2016-12-03 11:31:19 +0200

    hakee koordinaateista lähimmän kaupungin nimen

--- server.js
+++ server.js
@@ -32,13 +32,15 @@ router.get(/vedenlaatu/i, haeVedenlaatuApista)
 
 router.get("/paikka", haeKaikkiPaikat)
 
-router.get("/weather", getWeather)
+//router.get("/weather", getWeather)
+
+router.get(/nearestcity/, getNearestCity)
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
 
-function getWeather(req,res,next){
+/* function getWeather(req,res,next){
 	console.log("get weather called");
 	var YQL = require('yql');
 
@@ -50,6 +52,50 @@ function getWeather(req,res,next){
 	  
 	  console.log('The current weather in ' + location.city + ', ' + location.region + ' is ' + condition.temp + ' degrees.');
 	});
+} */
+
+//käytä näin
+//http://localhost:3000/getnearestcity?lati=62.614806&longi=28.618057
+function getNearestCity(req,res,next){
+	console.log("get nearest city");
+	var key="AIzaSyCkYwq88Z6EjbAUpeXxXbGEBYJyQHvVH7g"
+	var lati = req.query.lati;
+	var longi = req.query.longi;
+	var https = require('https');
+	console.log("lati" + lati +"longi" +longi)
+	var options = {
+	  host: 'maps.googleapis.com',
+	  path: '/maps/api/place/nearbysearch/json?location='+lati+',%20'+longi+'&type=cities&radius=50000&key=AIzaSyCkYwq88Z6EjbAUpeXxXbGEBYJyQHvVH7g'
+	};
+	
+	var req = https.get(options, function(rs) {
+	  console.log('STATUS: ' + res.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+	  // Buffer the body entirely for processing as a whole.
+	  var bodyChunks = [];
+	  rs.on('data', function(chunk) {
+		// You can process streamed parts here...
+		bodyChunks.push(chunk);
+		
+	  }).on('end', function() {
+		var body = Buffer.concat(bodyChunks);
+		console.log('BODY: ' + body);
+		var bodyJSON = JSON.parse(body);
+		var city = bodyJSON.results[0].name;
+		//console.log(city);
+		res.write(city);
+		res.end();
+		
+	  })
+	});
+	req.on('error', function(e) {
+	 console.log('ERROR: ' + e.message);
+	});
+		
+	
+	req.end();
+	
 }
 
 function haeKaikkiPaikat(req,res,next){
